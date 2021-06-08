1c9eb3c99df2e0970007ec342ebe1b1158d6a074
Sapunkov Alexey <sapalks@gmail.com>
2021-06-08 22:53:42 +0400

    fix webpack

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
   },
   resolve: {
